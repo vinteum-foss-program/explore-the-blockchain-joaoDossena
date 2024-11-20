e751a3ef66b4c0f57f9866b4629d8a2855f69458
joaoDossena <joaopdossena@gmail.com>
2024-11-20 18:19:12 -0300

    Script 3 - Getting number of outputs in the transactions of a block

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,10 @@
 # How many new outputs were created by block 123,456?
+txs=$(bitcoin-cli getblockhash 123456 | xargs bitcoin-cli getblock | jq '.tx.[]')
+n_outputs=0
+for i in $txs
+do
+i=$(echo $i | tr -d '"')
+tmp=$(bitcoin-cli getrawtransaction $i | xargs bitcoin-cli decoderawtransaction | jq '.vout | length')
+n_outputs=$(($n_outputs+$tmp))
+done
+echo $n_outputs
