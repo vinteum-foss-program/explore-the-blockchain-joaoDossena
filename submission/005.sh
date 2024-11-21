0579ceb4801455f36a5f7f6dc389f7c91c2b4316
joaoDossena <joaopdossena@gmail.com>
2024-11-21 15:11:26 -0300

    Removed p2sh-segwit argument

--- submission/005.sh
+++ submission/005.sh
@@ -4,5 +4,5 @@
 rawTx=$(bitcoin-cli getrawtransaction "37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517")
 txinwitnessAddr=$(bitcoin-cli decoderawtransaction $rawTx | jq -r '[.vin | .[] | .txinwitness | .[1]]')
 
-bitcoin-cli createmultisig 1 "$txinwitnessAddr" "p2sh-segwit" | jq '.address' | tr -d '"'
+bitcoin-cli createmultisig 1 "$txinwitnessAddr" | jq '.address' | tr -d '"'
 
